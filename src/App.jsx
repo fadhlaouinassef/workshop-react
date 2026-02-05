e59d4a21d2c34404cb2ffb4a8d352ef1c25b00de
fadhlaouinassef <nasseffadhlaoui@gmail.com>
2026-02-05 17:51:46 +0100

    Tp props et state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import './App.css'
-import './Ecmascript/exercices.js'
-import './Ecmascript/fonction.js'
+import Home from './components/Home.jsx'
+import Class from './components/Class.jsx'
+import Component from './components/Component.jsx'
+import Counter from './components/Counter.jsx'
+import ListManager from './components/ListManager.jsx'
+import ColorBox from './components/ColorBox.jsx'
+import NotesManager from './components/NotesManager.jsx'
+import TodoList from './components/TodoList.jsx'
 
 function App() {
   return (
     <div className="App">
-      <h1>Atelier ES6</h1>
-      <p>Ouvrez la console du navigateur (F12) pour voir les résultats des exercices</p>
+      <h1>Exercice 1</h1>
+      <Counter />
+      <h1>Exercice 2</h1>
+      <ListManager />
+      <h1>Exercice 3</h1>
+      <ColorBox />
+      <h1>Exercice 4</h1>
+      <NotesManager initialNotes={[12, 15.5, 8, 17]} />
+      <h1>Exercice 5</h1>
+      <TodoList initialTasks={[
+        { name: "Préparer le TD React", priority: "Haute" },
+        { name: "Faire les courses", priority: "Moyenne" },
+        { name: "Appeler le dentiste", priority: "Basse" },
+      ]} />
     </div>
   )
 }
